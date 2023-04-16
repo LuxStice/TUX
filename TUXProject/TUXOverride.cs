8be14a9148caca80182c7e229bf2ecdb5e797e2f
Pedro Costa <pedrocosta@infinitygames.io>
2023-04-16 19:30:02 +0100

    Added Override property

---
+++ TUXProject/TUXOverride.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace TUX;
+
+public partial class TUXPlugin
+{
+    public class TUXOverride
+    {
+        public TUXShaders.TUXShader baseShader { get; private set; }
+        public List<TUXProperty> overrides = new();
+        public (string partName, string targetGameObject) target;
+
+        public TUXOverride(TUXShaders.TUXShader tUXShader)
+        {
+            this.baseShader = tUXShader;
+        }
+
+        public Material GetMaterial()
+        {
+            Material m = baseShader.GetMaterial();
+            foreach(var over in overrides)
+            {
+                over.Apply(ref m);
+            }
+            return m;
+        }
+    }
+}
